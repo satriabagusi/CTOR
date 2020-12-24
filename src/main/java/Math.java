836c55259efde09789fddcb9043801e5e8b0b87a
Satria Bagus <satria.bagus18@gmail.com>
2020-12-24 15:25:48 +0700

    firs init

---
+++ src/main/java/Math.java
@@ -0,0 +1,15 @@
+
+public class Math {
+
+	private int n1, n2;
+	private double n;
+	
+	public void mod(int n1, int n2) {
+		this.n1 = n1;
+		this.n2 = n2;
+	}
+	
+	public void sqrt(double n) {
+		this.n = n;
+	}
+}
